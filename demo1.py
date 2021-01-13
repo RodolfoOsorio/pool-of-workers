d8e8e52580b4ec1f24b259b29910b91bf0483178
Rodolfo <jrodosorio@gmail.com>
2021-01-13 16:54:44 -0600

    New function added

--- demo1.py
+++ demo1.py
@@ -1,14 +1,32 @@
 from multiprocessing import Pool
 import numpy
 from time import time
-
 def mySquare():
     for k in range(50000):
         a = numpy.random.rand(40,40)
         b = numpy.random.rand(40,40)
         c = numpy.matmul(a,b)
 
-start_time = time()
-mySquare()
-elapsed_time = time() - start_time
-print("elapsed time: ", elapsed_time)
+def mySquare2():
+    for k in range(10000):
+        a = numpy.random.rand(40,40)
+        b = numpy.random.rand(40,40)
+        c = numpy.matmul(a,b)
+
+if __name__ == '__main__':
+    start_time = time()
+    mySquare()
+    elapsed_time = time() - start_time
+    print("elapsed time: ", elapsed_time)
+
+    # Runs 5 functions with 10000 operations each one
+
+    input("Press ENTER to multiprocessing")
+
+    p = Pool(5)             # number of workers
+    start_time = time()
+    p.map(mySquare2,())     # create copies of the function and assigns them to each worker
+    elapsed_time = time() - start_time
+    p.close()
+    p.join()
+    print("elapsed time: ", elapsed_time)
